168a5617aaf133d79cc0232aec3c6c29c6ff538c
Ericnguyen89 <127569176+Ericnguyen89@users.noreply.github.com>
2023-06-09 15:40:58 +0700

    Create Linux-client-install.sh

---
+++ Linux-client-install.sh
@@ -0,0 +1,60 @@
+if [[ -f /etc/os-release ]]; then
+    . /etc/os-release
+    OS=$ID
+else
+    echo "Failed to detect the operating system."
+    exit 1
+fi
+if [[ "$OS" == "ubuntu" ]]; then
+    apt-get update -y
+    apt-get upgrade -y
+elif [[ "$OS" == "centos" ]]; then
+    yum update -y
+else
+    echo "Unsupported operating system."
+    exit 1
+fi
+wget https://github.com/Velocidex/velociraptor/releases/download/v0.6.9/velociraptor-v0.6.9-linux-amd64
+mkdir /etc/velociraptor
+cp velociraptor-v0.6.9-linux-amd64 /usr/local/bin/velociraptor
+sudo chmod +x /usr/local/bin/velociraptor
+#write a service to start Velociraptor client
+echo "[Unit]
+Description=Velociraptor linux amd64
+After=syslog.target network.target
+[Service]
+Type=simple
+Restart=always
+RestartSec=120
+LimitNOFILE=20000
+Environment=LANG=en_US.UTF-8
+ExecStart=/usr/local/bin/velociraptor --config /etc/velociraptor/client.config.yaml client -v
+[Install]
+WantedBy=multi-user.target" > /lib/systemd/system/velociraptor-client.service
+
+config_file="/etc/velociraptor/server.config.yaml"
+service_file="/lib/systemd/system/velociraptor.service"
+
+# Check if the config file exists
+if [ -e "$config_file" ]; then
+  echo "All Working, please make a status of service to double check and sue not happend"
+  sudo systemctl start velociraptor-client
+else
+  echo "
+  ------------------------
+  
+  DEFAULT CONFIG OF VELOCIRAPTOR CLIENT NOT FOUND IN [/etc/velociraptor/client.config.yaml]
+  YOU NEED TO CHANGE IT:
+  
+  "
+  
+  # Prompt the user for the replacement value
+  read -p "Enter the replacement config file value for Velociraptor client file you download look like [/etc/velociraptor/client.config.yaml]:" replacement
+  
+  
+  # Replace the value in the service file
+  sed -i "s|/etc/velociraptor/client.config.yaml|$replacement|" "$service_file"
+  
+  echo "Complete setup a server system service for Velociraptor, Let start Velociraptor and Enjoy!"
+    
+fi
