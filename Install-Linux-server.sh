e7ebaf135774f540760c5375d61e52626778702b
Ericnguyen89 <127569176+Ericnguyen89@users.noreply.github.com>
2023-07-27 15:24:22 +0700

    Update Install-Linux-server.sh

--- Install-Linux-server.sh
+++ Install-Linux-server.sh
@@ -54,53 +54,12 @@ sudo systemctl enable --now velociraptor
 #Seach config localhost and replace new value of IP
 #!/bin/bash
 
-# Read the file content
-file_path="/etc/velociraptor/server.config.yaml"
-file_content=$(cat "$file_path")
-
-# Find occurrences of '127.0.0.1' in the file
-ip_count=$(grep -c '127.0.0.1' "$file_path")
-
-if [ "$ip_count" -eq 0 ]; then
-  echo "No occurrences of '127.0.0.1' found in the file."
-  echo "There is no value to replace."
-else
-  # Generate IP addresses for selection
-  ip_addresses=($(hostname -I))
-  ip_count=${#ip_addresses[@]}
-
-  echo "Select the IP address to replace default value of localhost '127.0.0.1':"
-
-  # Print IP addresses with corresponding numbers
-  for ((i=0; i<ip_count; i++)); do
-    echo "$(($i+1)). ${ip_addresses[$i]}"
-  done
-
-  # Read the user's choice
-  read -p "Enter the number of the IP address to use (1-$ip_count): " choice
-
-  if [[ $choice =~ ^[1-$ip_count]$ ]]; then
-    # Replace '127.0.0.1' with the chosen IP address
-    new_ip="${ip_addresses[$(($choice-1))]}"
-    updated_content="${file_content//127.0.0.1/$new_ip}"
-
-    # Write the updated content back to the file
-    echo "$updated_content" > "$file_path"
-    echo "Value replaced successfully."
-  else
-    echo "Invalid choice. No changes were made."
-  fi
-fi
-#-----------------------------------------------
-# seaching config of velociraptor server to double check after start services
-#!/bin/bash
-
 config_file="/etc/velociraptor/server.config.yaml"
 service_file="/lib/systemd/system/velociraptor.service"
 
 # Check if the config file exists
 if [ -e "$config_file" ]; then
-  echo "All Working, please make a status of service to double check and sue not happend"
+  echo "All Working, please make a status of service to double check and sure not happend"
 else
   echo "
   ------------------------
